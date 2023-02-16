9f9c36fa9c0d5bf7e39764bf776af6680b37aa77
ljacques99 <laurent.jacques@evolvers.fr>
2023-02-16 18:32:20 +0000

    logo in browser tab

--- frontend/src/Components/Welcome.jsx
+++ frontend/src/Components/Welcome.jsx
@@ -6,7 +6,7 @@ export function Welcome() {
     return <div className="d-flex justify-content-center">
         <div className="d-flex flex-column w-50">
             <div className='align-self-center'>
-                <img style={{ width: 300, height: 300 }} src={ ethLogo}></img>
+                <img style={{ width: 200, height: 200 }} src={ ethLogo}></img>
             </div>
             <h2 className="text-center">Bienvenido a Build Private Ethereum Networks</h2>
         
