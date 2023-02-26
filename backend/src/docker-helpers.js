8fe37e805a422fba5f221fed19c81891d11895e8
ljacques99 <laurent.jacques@evolvers.fr>
2023-02-26 21:06:53 +0000

    domingo 2206

--- backend/src/docker-helpers.js
+++ backend/src/docker-helpers.js
@@ -260,11 +260,11 @@ function createContainerBootNodeEnode(imageId, networkid){
         '/opt/bootnode': {}
       },
       'HostConfig': {
-        'PortBindings' : {"8010/tcp" : [{"HostPort": "8012"}]},
+        'PortBindings' : {"8010/tcp" : [{"HostPort": "8015"}]},
         'Binds': [`${absolutePath}/Ethereum/network${networkid}/:/opt/bootnode`]
       },
       ExposedPorts:{
-        "8012/tcp":{}
+        "8015/tcp":{}
       },
       User:""
     },(err,stream)=>{
