c61a0b6ddf3ee79f1a21afd1d631072485cd7894
ljacques99 <laurent.jacques@evolvers.fr>
2023-03-01 15:13:03 +0000

    corrected launchNode

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 const morgan = require("morgan")
 const app = express()
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log("Listening ", PORT)
 })
 
